2a07800d21f6e98366a74487a734d8b083ea2f63
majidzzoda <majidzzoda@gmail.com>
2025-02-15 17:53:33 +0500

    added

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # string-methods
+String и методы String в JavaScript
+String в JavaScript
+String — это последовательность символов, заключенная в кавычки (' ', " ", `` `).
+
+Основные методы работы со String:
+length — возвращает длину String.
+
+let str = "Hello";
+console.log(str.length); // 5
+toUpperCase() / toLowerCase() — изменяет регистр.
+
+
+console.log("hello".toUpperCase()); // "HELLO"
+console.log("WORLD".toLowerCase()); // "world"
+trim() — удаляет пробелы с начала и конца.
+
+
+console.log("  text  ".trim()); // "text"
+charAt(index) — возвращает символ по индексу.
+
+console.log("JavaScript".charAt(4)); // "S"
+indexOf(substring) / lastIndexOf(substring) — ищет подстроку в String.
+
+
+console.log("Hello, world".indexOf("world")); // 7
+includes(substring) — проверяет, содержится ли подстрока в String.
+
+
+console.log("JavaScript".includes("Script")); // true
+slice(start, end) — возвращает часть String.
+
+console.log("Hello".slice(1, 4)); // "ell"
+replace(old, new) — заменяет часть String.
+
+console.log("Hello world".replace("world", "JS")); // "Hello JS"
+split(delimiter) — разбивает String в массив.
+
+console.log("a,b,c".split(",")); // ["a", "b", "c"]
+repeat(n) — повторяет String n раз.
+
+console.log("Hi ".repeat(3)); // "Hi Hi Hi "
+Эти методы помогают эффективно работать со String, обрабатывать текст и выполнять манипуляции с данными. 🚀
---
+++ index.js
@@ -0,0 +1,88 @@
+// function getInfo(name, age) {
+//     return `My name is ${name} and my age is ${age} years old.`;
+// }
+// console.log(getInfo("Ahmad", 19));
+
+// console.log(text.length);
+
+// let matn = "dafsha_sag";
+// console.log(matn[0]);
+
+// let text = "hello ahmad";
+// // for (let i = 0; i < text.length; i++) {
+// //     console.log(text[i]);
+// // }
+// for (let element of text) {
+//     console.log(element);
+// }
+
+// function sum(str) {
+//     let cnt = null;
+//     for (let element of str) {
+//         if (element != ',') {
+//             cnt += +element
+//         }
+//     }
+//     return cnt;
+// }
+// console.log(sum("2,3,4,5,6"));
+
+// let text = "Hello World"
+// console.log(text.charAt(0));
+
+// let num = 59;
+// console.log(num.toString(2));
+
+// let text1 = "hello";
+// let text2 = "bots";
+// console.log(text1.concat(" ", text2));
+
+// let text = "hello bots";
+// let res = text.replace("bots", "world")
+// console.log(res;);
+
+// let text = "hello legends";
+// let res = text.substring(1);
+// console.log(res);
+
+// let text = "hello world stand-up maybe i will";
+// let res = text.split(" ");
+// console.log(res);
+
+// function modifyLast(a, b) {
+//     let last = a.at(-1)
+//     let zaragul = last.repeat(b-1);
+//     return a+zaragul; 
+// }
+// console.log(modifyLast("Hello", 3));
+// console.log(modifyLast("hey", 6));
+// console.log(modifyLast("excuse me what?", 5));  
+
+// function firstLast(a) {
+//     return a.at(0)+a.at(-1)
+// }
+// console.log(firstLast("ganesh"));
+// console.log(firstLast("shiva"));
+
+// function toInt(str) {
+//     return parseInt(str, 10);
+// }
+// function toStr(num) {
+//     return num.toString();
+// }
+
+// function isPlural(a) {
+//     if (a.at(-1) == 's') return true
+//     else return false
+// }
+// console.log(isPlural("changes"));
+// console.log(isPlural("change"));
+// console.log(isPlural("dudes"));
+
+// function reverseCapitalize(a) {
+//     return a.toUpperCase();
+// }
+// console.log(reverseCapitalize("abc"));
+// console.log(reverseCapitalize("hellothere"));
+// console.log(reverseCapitalize("input"));
+
